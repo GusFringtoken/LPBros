5e7695db0dc12341f54a998cfc080d4838d37835
RVMsgit <gusfring.token@gmail.com>
2023-05-14 17:21:03 +0200

    test masnolehanavisado_v2

--- noticias.js
+++ noticias.js
@@ -1,3 +1,14 @@
 function cargarPagina(url) {
   window.location.href = url;
 }
+
+function aceptarPopup() {
+  var audio = new Audio("masnolehanavisado.mp3");
+  audio.play();
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+window.onload = function() {
+  mostrarPopup();
+}
